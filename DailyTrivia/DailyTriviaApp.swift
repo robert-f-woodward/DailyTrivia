8c12c0a9f276790ebd32d79abc8d59c43d619ae0
Bob Woodward <robert.f.woodward@gmail.com>
2025-08-16 07:19:54 -0500

    Initial Commit

---
+++ DailyTrivia/DailyTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTriviaApp.swift
+//  DailyTrivia
+//
+//  Created by Robert Woodward on 8/16/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
